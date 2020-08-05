2fd708163a67cc2eb4b9b68e238e87a699a4fd90
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 13:34:27 +0200

    Test Commit

--- bot.js
+++ bot.js
@@ -11,6 +11,9 @@ bot.on('message', msg=>{
     if(msg.content.toLowerCase() === 'hello'){
         msg.reply('Ni geniete');
     }
-})
+    if(msg.content.toLowerCase === 'czn'){
+        msg.channel.send('Ewa');
+    }
+});
 
 bot.login(token);
