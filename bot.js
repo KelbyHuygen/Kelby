ee75ad661968fb434730988ff3a134b2df73d243
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 13:38:47 +0200

    Test Commit

--- bot.js
+++ bot.js
@@ -5,15 +5,15 @@ const token = 'NzQwNTEyNjY5ODM2MjQ3MTAx.XyqGKg.xDkxM-Z201i_yUYgVhL2Te3RmP0';
 
 bot.on('ready', () => {
     console.log('This bot is online!');
-})
+});
 
 bot.on('message', msg=>{
     if(msg.author.bot)
     return;
 
-    if(msg.content.toLowerCase() === 'hello'){
+    if(msg.content.toLowerCase() === 'hello')
         msg.reply('Ni geniete');
-    }
+        
     if(msg.content.toLowerCase === 'czn'){
         msg.channel.send('Ewa');
     }
