f0f1b5e6562a43662db537d39add36cef9eb4cc2
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 12:06:28 +0200

    Test Commit

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NzM1MTEyNjg1NTM2MDE4NTYy.XxbhCQ.tUMPFQYUotCZLLWEzAuuaFDCDoM';
+const token = 'NzM1MTEyNjg1NTM2MDE4NTYy.XxbhCQ.qy7j6q5E6nxWbp9it7nbKzhU1Ck';
 
 bot.on('ready', () => {
     console.log('This bot is online!');
