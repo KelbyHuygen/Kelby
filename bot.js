188dceb7997a3c9deed4de7d718e8b5a3f36e9ce
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 13:17:37 +0200

    Test Commit

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ bot.on('ready', () => {
 })
 
 bot.on('message', msg=>{
-    if(msg.content === "HELLO"){
-        msg.reply('Hello');
+    if(msg.content.toLowerCase() === "wtf maat"){
+        msg.reply('U dikke bomma maat');
     }
 })
 
