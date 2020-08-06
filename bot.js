8fb316da15831bb157b26bc8bcb834a8beeeb7d1
KelbyHuygen <unconfigured@null.spigotmc.org>
2020-08-07 00:04:58 +0200

    Test Commitµ

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 const token = 'NzQwNTEyNjY5ODM2MjQ3MTAx.XyqGKg.xDkxM-Z201i_yUYgVhL2Te3RmP0';
-
+const version = '1.2.0';
 const prefix = '?';
 bot.on('message', msg=>{
 
@@ -12,6 +12,10 @@ bot.on('message', msg=>{
         case 'ping':
             msg.reply('pong');
             break;
+
+            case 'info':
+                msg.channel.send('Version' + version );
+                break;
     }
 })
 bot.on('ready', () => {
