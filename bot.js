66130cd63e7ff1a58b371bc4ad3f0e9c9a773a29
KelbyHuygen <unconfigured@null.spigotmc.org>
2020-08-07 00:15:31 +0200

    Test Commitµ

--- bot.js
+++ bot.js
@@ -11,12 +11,10 @@ bot.on('message', msg=>{
     switch(args[0]){
         case 'ping':
             msg.reply('pong');
-            const description = "pong";
             break;
 
         case 'info':
             msg.channel.send('Version ' + version );
-            const description = "Shows the current version";
             break;
         
         case 'help':
